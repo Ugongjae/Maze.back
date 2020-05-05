81bf56440eb74000ac5b17d54735d4985993e03c
Ugongjae <turtle0191@naver.com>
2020-05-06 01:31:08 +0900

    jwt prototype

---
+++ src/main/java/com/maze/back/security/UserPrincipalDetailsService.java
@@ -0,0 +1,24 @@
+package com.maze.back.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserPrincipalDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username);
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+
+        return userPrincipal;
+    }
+}
